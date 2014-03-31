10037bc7a4e8276c13258c9f5affb7f824380036
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-31 11:25:52 -0500

    pass tests on Car and Person constructors

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,19 @@
 (function () {
 
-  window.Car = function () {
-    // TODO
+  window.Car = function (color, person) {
+    this.state = "off"
+    this.color = color;
+    this.driver = person
+    this.ignite = function(){
+      if (this.driver === undefined){
+        return "There is no driver to start the car!"
+      } else if (this.driver.hasDriversLicense === false) {
+        return "Sorry, you do not have a license."
+      } else {
+      this.state = "running";
+      return "Vroom!";
+      };
+    };
   };
 
 })();
--- src/Person.js
+++ src/Person.js
@@ -1,7 +1,11 @@
 (function () {
 
   window.Person = function (name) {
-    // TODO
+    this.name = name;
+    this.hasDriversLicense = false;
+    this.receiveLicense = function(){
+      this.hasDriversLicense = true;
+    };
   };
 
 })();
