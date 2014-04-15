0485a575ee4561d8bd3bf3299ec720bc26b16ecb
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-04-15 11:03:38 -0500

    Update render function

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,10 @@
-var render = function(){
+var render = function(template, data){
 
+  var renderedTemp = template;
+
+  for (var prop in data){
+    renderedTemp = renderedTemp.replace(/{(.*?)}}/, data[prop]);
+  }
+
+  return renderedTemp;
 };
